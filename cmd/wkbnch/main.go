5c61474ee96512c37c7a58f55c5cd679d9a1af44
Liam Kinne <liam@kinne.io>
2022-11-13 21:00:55 +1000

    working parsing and validation

--- cmd/wkbnch/main.go
+++ cmd/wkbnch/main.go
@@ -7,17 +7,19 @@ import (
 )
 
 type Config struct {
-	In   *string
-	Out  *string
-	Lang *string
+	In       *string
+	Out      *string
+	Lang     *string
+	Validate *bool
 }
 
 func main() {
 	// configure
 	config := Config{
-		In:   flag.String("in", "", "input file or directory"),
-		Out:  flag.String("out", "", "output directory"),
-		Lang: flag.String("lang", "c", "language to generate"),
+		In:       flag.String("in", "", "input file or directory"),
+		Out:      flag.String("out", "", "output directory"),
+		Lang:     flag.String("lang", "c", "language to generate"),
+		Validate: flag.Bool("validate", false, "don't output, only validate input files"),
 	}
 
 	flag.Parse()
@@ -39,6 +41,13 @@ func main() {
 	objects, err := objectsFromFilenames(files)
 	if err != nil {
 		log.Fatalf("failed parsing in files: %v", err)
+	}
+
+	for _, obj := range objects {
+		err = obj.Validate()
+		if err != nil {
+			log.Fatalf("failed validation: %v", err)
+		}
 	}
 
 	log.Printf("parsed in %d objects from %d files", len(objects), len(files))
